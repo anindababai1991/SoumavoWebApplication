6c5f2a8f5bfa7f22efd6151dba0a16f16766ac04
aninda.basu <basu.anindabasu.aninda@gmail.com>
2017-07-26 18:25:25 +0530

    enabled swagger for api management inn azure

--- Soumavo.APILayer/App_Start/SwaggerConfig.cs
+++ Soumavo.APILayer/App_Start/SwaggerConfig.cs
@@ -20,7 +20,7 @@ namespace Soumavo.APILayer
                         // By default, the service root url is inferred from the request used to access the docs.
                         // However, there may be situations (e.g. proxy and load-balanced environments) where this does not
                         // resolve correctly. You can workaround this by providing your own code to determine the root URL.
-                        //
+                        c.OperationFilter<UniqueOperation>();
                         //c.RootUrl(req => GetRootUrlFromAppConfig());
 
                         // If schemes are not explicitly provided in a Swagger 2.0 document, then the scheme used to access
---
+++ Soumavo.APILayer/UniqueOperation.cs
@@ -0,0 +1,36 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace Soumavo.APILayer
+{
+    /// <summary>
+    /// unique Operation for swagger
+    /// </summary>
+    public class UniqueOperation : IOperationFilter
+    {
+        /// <summary>
+        /// Apply Unique Operation
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiDescription"></param>
+        public void Apply(
+            Operation operation,
+            SchemaRegistry schemaRegistry,
+            ApiDescription apiDescription)
+        {
+            if (operation.parameters != null)
+            {
+                operation.operationId += "By";
+                foreach (var parm in operation.parameters)
+                {
+                    operation.operationId += string.Format("{0}", parm.name);
+                }
+            }
+        }
+    }
+}
